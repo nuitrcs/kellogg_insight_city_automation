944afcd93d36fee4b2c050ae2d09764b733625c6
felavsky <elavsky@northwestern.edu>
2018-04-09 11:49:43 -0500

    added commas to displayed employment pop, fixed top text

--- scripts/mods/map.js
+++ scripts/mods/map.js
@@ -20,6 +20,12 @@ for (var num = 0; num < tilde.cities.length; num++) {
   var d = tilde.cities[num];
   var place_lat = d["Lat"];
   var place_long = d["Lng"];
+  var e = d.e.toString()
+
+  if (e.length >= 5) {
+    e = e.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+  }
+  d.e_display = e
 
   var circle = L.circle([place_lat, place_long], {
       color: tilde.colorScale(d.ai),
@@ -40,7 +46,7 @@ tilde.map.move = function(lat,lng) {
 tilde.map.mark = function(data) {
   tilde.map.markerGroup.clearLayers()
   tilde.marker = L.marker([data.Lat, data.Lng]).addTo(tilde.map.markerGroup)
-  var popup_html = '<h3><b>' + data['Location'] + '</b></h3><p><b>Automation impact</b>: '+round(data.ai*100,2)+'%<br><b>Affected Employment</b>: '+data.e+'</p>';
+  var popup_html = '<h3><b>' + data['Location'] + '</b></h3><p><b>Automation impact</b>: '+round(data.ai*100,2)+'%<br><b>Affected Employment</b>: '+data.e_display+'</p>';
   tilde.marker.bindPopup(popup_html);
   tilde.marker.openPopup()
 }
